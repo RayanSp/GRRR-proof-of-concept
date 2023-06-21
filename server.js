0b42ceaca9a51aa428da091f143af481f9aa4f49
RayanSp <rayan.spall@hva.nl>
2023-06-21 11:15:21 +0200

    collapsable content

--- server.js
+++ server.js
@@ -1,103 +1,66 @@
 // Importeert basis modules uit npm
 
 import express from "express";
-
 import dotenv from "dotenv";
 
-
-
-
-dotenv.config();
-
-
-
+// Defineer een URL
+const url =
+  "https://s3.eu-west-1.amazonaws.com/data.theoceancleanup.com/systems-dashboard/realtime-data-river.json";
 
 // Maakt een nieuwe express app
 
 const server = express();
 
-
-
-
 // Stelt de public map in
 
 server.use(express.static("public"));
-
-
-
 
 // Stelt het poortnummer in waar express op gaat luisteren
 
 server.set("port", process.env.PORT || 8000);
 
-
-
-
 // Activeert het .env bestand
 
 dotenv.config();
 
-
-
-
 // Stel in hoe express gebruikt kan worden
 
 server.set("view engine", "ejs");
-
 server.set("views", "./views");
+server.use(express.static("public"));
 
+// Route voor de index
 
+server.get("/", (request, response) => {
+  let indexUrl = url + "/";
 
+  fetchJson(indexUrl).then((data) => {
+    response.render("index", data);
+  });
+});
+
+server.get("/oceans", (request, response) => {
+  let oceansUrl = url + "/oceans";
+
+  fetchJson(oceansUrl).then((data) => {
+    response.render("oceans", data);
+  });
+});
+
+server.get("/rivers", (request, response) => {
+    let riversUrl = url + "/rivers";
+  
+    fetchJson(riversUrl).then((data) => {
+      response.render("rivers", data);
+    });
+  });
 
 // Start express op, haal het ingestelde poortnummer op
 
+server.set("port", process.env.PORT || 8000);
 server.listen(server.get("port"), function () {
-
-    // Toon een bericht in de console en geef het poortnummer door
-
-    console.log(
-
-        `Application started on http://localhost:${server.get(
-
-            "port"
-
-        )}`
-
-    );
-
+  console.log(`Application started on http://localhost:${server.get("port")}`);
 });
-
-
-
-
-// Maakt een route voor de index
-
-server.get("/", (request, response) => {
-
-
-
-
-    // let defaultUrl = ""
-
-    // fetchJson(defaultUrl).then((data) => {
-
-    //  response.render("index", data);
-
-    // });
-
-
-
-
-    response.render("index");
-
-
-
-
-});
-
-
-
-
 
 /**
 
@@ -112,13 +75,10 @@ server.get("/", (request, response) => {
 */
 
 export async function fetchJson(url) {
-
-return await fetch(url)
-
+  return await fetch(url)
     .then((response) => response.json())
 
-    .catch((error) => error)
-
+    .catch((error) => error);
 }
 
-export default server
+export default server;
